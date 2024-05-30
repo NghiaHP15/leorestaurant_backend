762caeea13edf34dbed9f377155bf476218e98a8
nghiakuter002 <Nghiakuter002@gmail.com>
2024-05-30 18:26:18 +0700

    fix last

--- src/controllers/CustomerController.js
+++ src/controllers/CustomerController.js
@@ -25,7 +25,7 @@ const updateCustomer = async (req, res) => {
     if (!customerId) {
       return res.status(200).json({
         status: "ERR",
-        message: "The Customer Id is required",
+        message: "The Customer Id is requireds",
       });
     }
     const response = await CustomerService.updateCustomer(customerId, data);
